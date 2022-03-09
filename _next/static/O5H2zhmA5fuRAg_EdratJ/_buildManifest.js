fb627567991429ed5bc806d8a0f2eabe7bd276ac
janakhpon <42414925+janakhpon@users.noreply.github.com>
2022-03-09 02:02:28 +0000

    Deploying to gh-pages from @ janakhpon/seo-demo@51c9a8ba84ac09efa1c0c820eaa6cb35c63a9336 🚀

---
+++ _next/static/O5H2zhmA5fuRAg_EdratJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-a181fdfa4ee3f569.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],"/members/[name]":["static/chunks/pages/members/[name]-0a58f6e5a78f31ce.js"],sortedPages:["/","/_app","/_error","/members/[name]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
